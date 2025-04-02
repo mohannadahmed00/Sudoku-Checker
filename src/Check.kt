799b07c5374f7674a24904e6f91e12021c7b8fda
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-02 21:44:07 +0200

    Add Sudoku checker logic

--- src/Check.kt
+++ src/Check.kt
@@ -1,7 +1,7 @@
 /*
     After completing the instructional videos, implement a Sudoku checker function that determines whether a given Sudoku puzzle is valid or not.
     A valid Sudoku must not contain any repeated numbers in the same row, column, or 3x3 subgrid (box).
-    Use the character '-' to represent empty cells within the puzzle.
+    Use the character "-" to represent empty cells within the puzzle.
 
     - The function should return a Boolean value only—true if the Sudoku is valid, and false otherwise.
 
@@ -16,20 +16,18 @@
  */
 
 fun main() {
-
-
     check(
         name = "when enter an empty sudoku return true", result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-'),
-                listOf('-', '-', '-', '-', '-', '-', '-', '-', '-')
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-"),
+                listOf("-", "-", "-", "-", "-", "-", "-", "-", "-")
             )
         ), correctResult = true
     )
@@ -37,15 +35,15 @@ fun main() {
         name = "when enter a completed sudoku not contains any repeated numbers in the same row, column, or 3x3 subgrid (box) return true",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                listOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
-                listOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
-                listOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
-                listOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
-                listOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
-                listOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
-                listOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
-                listOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         correctResult = true
@@ -54,15 +52,15 @@ fun main() {
         name = "when enter an uncompleted sudoku not contains any repeated numbers in the same row, column, or 3x3 subgrid (box) return true",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '-', '-', '7', '-', '-', '-', '-'),
-                listOf('6', '-', '-', '1', '9', '5', '-', '-', '-'),
-                listOf('-', '9', '8', '-', '-', '-', '-', '6', '-'),
-                listOf('8', '-', '-', '-', '6', '-', '-', '-', '3'),
-                listOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
-                listOf('7', '-', '-', '-', '2', '-', '-', '-', '6'),
-                listOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
-                listOf('-', '-', '-', '4', '1', '9', '-', '-', '5'),
-                listOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
+                listOf("5", "3", "-", "-", "7", "-", "-", "-", "-"),
+                listOf("6", "-", "-", "1", "9", "5", "-", "-", "-"),
+                listOf("-", "9", "8", "-", "-", "-", "-", "6", "-"),
+                listOf("8", "-", "-", "-", "6", "-", "-", "-", "3"),
+                listOf("4", "-", "-", "8", "-", "3", "-", "-", "1"),
+                listOf("7", "-", "-", "-", "2", "-", "-", "-", "6"),
+                listOf("-", "6", "-", "-", "-", "-", "2", "8", "-"),
+                listOf("-", "-", "-", "4", "1", "9", "-", "-", "5"),
+                listOf("-", "-", "-", "-", "8", "-", "-", "7", "9")
             )
         ),
         correctResult = true
@@ -72,15 +70,15 @@ fun main() {
         name = "when enter a completed sudoku contains any repeated numbers in the same row return false",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                listOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
-                listOf('1', '9', '8', '3', '7', '2', '5', '6', '7'),
-                listOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
-                listOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
-                listOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
-                listOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
-                listOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
-                listOf('3', '4', '5', '2', '8', '6', '1', '7', '3')
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "7", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "3")
             )
         ),
         correctResult = false
@@ -89,15 +87,15 @@ fun main() {
         name = "when enter an uncompleted sudoku contains any repeated numbers in the same row return false",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                listOf('6', '7', '2', '1', '9', '5', '3', '4', '-'),
-                listOf('-', '9', '7', '-', '-', '-', '-', '6', '7'),
-                listOf('8', '-', '-', '-', '6', '-', '-', '-', '3'),
-                listOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
-                listOf('7', '-', '-', '-', '2', '-', '-', '-', '6'),
-                listOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
-                listOf('-', '-', '-', '1', '1', '9', '-', '-', '5'),
-                listOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "-"),
+                listOf("-", "9", "7", "-", "-", "-", "-", "6", "7"),
+                listOf("8", "-", "-", "-", "6", "-", "-", "-", "3"),
+                listOf("4", "-", "-", "8", "-", "3", "-", "-", "1"),
+                listOf("7", "-", "-", "-", "2", "-", "-", "-", "6"),
+                listOf("-", "6", "-", "-", "-", "-", "2", "8", "-"),
+                listOf("-", "-", "-", "1", "1", "9", "-", "-", "5"),
+                listOf("-", "-", "-", "-", "8", "-", "-", "7", "9")
             )
         ),
         correctResult = false
@@ -107,15 +105,15 @@ fun main() {
         name = "when enter a completed sudoku contains any repeated numbers in the same column return false",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                listOf('6', '7', '2', '1', '9', '5', '3', '4', '8'),
-                listOf('1', '9', '8', '3', '4', '3', '5', '6', '7'),
-                listOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
-                listOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
-                listOf('7', '1', '3', '9', '2', '4', '8', '5', '6'),
-                listOf('9', '6', '2', '5', '3', '7', '2', '8', '4'),
-                listOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
-                listOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "3", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "2", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "9")
             )
         ),
         correctResult = false
@@ -124,15 +122,15 @@ fun main() {
         name = "when enter an uncompleted sudoku contains any repeated numbers in the same column return false",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '-', '-', '7', '-', '-', '-', '-'),
-                listOf('6', '-', '-', '1', '9', '5', '-', '7', '-'),
-                listOf('-', '9', '8', '-', '-', '-', '-', '6', '-'),
-                listOf('8', '-', '-', '-', '6', '-', '-', '-', '3'),
-                listOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
-                listOf('7', '-', '8', '-', '2', '-', '-', '-', '6'),
-                listOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
-                listOf('-', '-', '-', '4', '1', '9', '-', '-', '5'),
-                listOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
+                listOf("5", "3", "-", "-", "7", "-", "-", "-", "-"),
+                listOf("6", "-", "-", "1", "9", "5", "-", "7", "-"),
+                listOf("-", "9", "8", "-", "-", "-", "-", "6", "-"),
+                listOf("8", "-", "-", "-", "6", "-", "-", "-", "3"),
+                listOf("4", "-", "-", "8", "-", "3", "-", "-", "1"),
+                listOf("7", "-", "8", "-", "2", "-", "-", "-", "6"),
+                listOf("-", "6", "-", "-", "-", "-", "2", "8", "-"),
+                listOf("-", "-", "-", "4", "1", "9", "-", "-", "5"),
+                listOf("-", "-", "-", "-", "8", "-", "-", "7", "9")
             )
         ),
         correctResult = false
@@ -142,15 +140,15 @@ fun main() {
         name = "when enter a completed sudoku contains any repeated numbers in the same subgrid (box) return false",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '4', '6', '7', '8', '9', '1', '2'),
-                listOf('6', '5', '2', '1', '9', '5', '3', '4', '8'),
-                listOf('1', '9', '8', '3', '4', '2', '5', '6', '7'),
-                listOf('8', '5', '9', '7', '6', '1', '4', '2', '3'),
-                listOf('4', '2', '6', '8', '5', '3', '7', '9', '1'),
-                listOf('7', '1', '3', '9', '2', '6', '8', '5', '6'),
-                listOf('9', '6', '1', '5', '3', '7', '2', '8', '4'),
-                listOf('2', '8', '7', '4', '1', '9', '6', '3', '5'),
-                listOf('3', '4', '5', '2', '8', '6', '1', '7', '9')
+                listOf("5", "3", "4", "6", "7", "8", "9", "1", "2"),
+                listOf("6", "7", "2", "1", "9", "5", "3", "4", "8"),
+                listOf("1", "9", "8", "3", "4", "2", "5", "6", "7"),
+                listOf("8", "5", "9", "7", "6", "1", "4", "2", "3"),
+                listOf("4", "2", "6", "8", "5", "3", "7", "9", "1"),
+                listOf("7", "1", "3", "9", "2", "4", "8", "5", "6"),
+                listOf("9", "6", "1", "5", "3", "7", "2", "8", "4"),
+                listOf("2", "8", "7", "4", "1", "9", "6", "3", "5"),
+                listOf("3", "4", "5", "2", "8", "6", "1", "7", "5")
             )
         ),
         correctResult = false
@@ -159,15 +157,52 @@ fun main() {
         name = "when enter an uncompleted sudoku contains any repeated numbers in the same subgrid (box) return false",
         result = isSudokuValid(
             sudokuBoard = listOf(
-                listOf('5', '3', '-', '-', '7', '-', '-', '-', '-'),
-                listOf('6', '-', '-', '1', '9', '5', '-', '-', '-'),
-                listOf('-', '9', '8', '-', '-', '-', '-', '6', '-'),
-                listOf('8', '-', '-', '-', '6', '-', '-', '-', '3'),
-                listOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
-                listOf('7', '-', '-', '-', '2', '-', '-', '-', '6'),
-                listOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
-                listOf('-', '-', '-', '4', '1', '9', '-', '2', '5'),
-                listOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
+                listOf("5", "3", "-", "-", "7", "-", "-", "-", "-"),
+                listOf("6", "-", "-", "1", "9", "5", "-", "-", "-"),
+                listOf("-", "9", "8", "-", "-", "-", "-", "6", "-"),
+                listOf("8", "-", "-", "-", "6", "-", "-", "-", "3"),
+                listOf("4", "-", "-", "8", "-", "3", "-", "-", "1"),
+                listOf("7", "-", "-", "-", "2", "-", "-", "-", "6"),
+                listOf("-", "6", "-", "-", "-", "-", "2", "8", "-"),
+                listOf("-", "-", "-", "4", "1", "9", "-", "2", "5"),
+                listOf("-", "-", "-", "-", "8", "-", "-", "7", "9")
+            )
+        ),
+        correctResult = false
+    )
+
+    check(
+        name = "when enter an empty 4*4 sudoku return true",
+        result = isSudokuValid(
+            sudokuBoard = listOf(
+                listOf("-", "-", "-", "-"),
+                listOf("-", "-", "-", "-"),
+                listOf("-", "-", "-", "-"),
+                listOf("-", "-", "-", "-"),
+            )
+        ),
+        correctResult = true
+    )
+    check(
+        name = "when enter an uncompleted 4*4 sudoku not contains any repeated numbers in the same row, column, or 3x3 subgrid (box) return true",
+        result = isSudokuValid(
+            sudokuBoard = listOf(
+                listOf("1", "3", "-", "-"),
+                listOf("2", "-", "-", "1"),
+                listOf("-", "4", "3", "-"),
+                listOf("3", "-", "-", "2"),
+            )
+        ),
+        correctResult = true
+    )
+    check(
+        name = "when enter an uncompleted 4*4 sudoku contains any repeated numbers in the same subgrid (box) return false",
+        result = isSudokuValid(
+            sudokuBoard = listOf(
+                listOf("1", "3", "-", "-"),
+                listOf("2", "-", "-", "4"),
+                listOf("-", "4", "3", "1"),
+                listOf("3", "-", "1", "2"),
             )
         ),
         correctResult = false
@@ -181,8 +216,8 @@ fun check(
     correctResult: Boolean,
 ) {
     if (result == correctResult) {
-        println("Success - $name")
+        println("\u001B[32m Success - $name \u001B[0m")
     } else {
-        println("Failed - $name")
+        println("\u001B[31m Failed - $name \u001B[0m")
     }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,73 @@
-fun main() {}
+import kotlin.math.sqrt
 
-fun isSudokuValid(sudokuBoard: List<List<Char>>): Boolean {
-    return false
+fun isSudokuValid(sudokuBoard: List<List<String>>): Boolean {
+    if (isEmptyBoard(sudokuBoard)) return true
+    if (!isValidRows(sudokuBoard)) return false
+    if (!isValidColumns(sudokuBoard)) return false
+    if (!isValidSubGrids(sudokuBoard)) return false
+    return true
 }
+
+fun isEmptyBoard(sudokuBoard: List<List<String>>): Boolean {
+    sudokuBoard.forEach { rows ->
+        rows.forEach { cell ->
+            if (cell != "-") return false
+        }
+    }
+    return true
+}
+
+fun isValidRows(sudokuBoard: List<List<String>>): Boolean {
+    sudokuBoard.forEach { row ->
+        val seen = mutableListOf<String>()
+        row.forEach { cell ->
+            if (isValidCell(cell, sudokuBoard.size, seen)) seen.add(cell)
+        }
+    }
+    return true
+}
+
+fun isValidColumns(sudokuBoard: List<List<String>>): Boolean {
+    for (columnIndex in sudokuBoard[0].indices) {
+        val seen = mutableListOf<String>()
+        sudokuBoard.forEach { row ->
+            val cell = row[columnIndex]
+            if (isValidCell(cell, sudokuBoard.size, seen)) seen.add(cell)
+        }
+    }
+    return true
+}
+
+fun isValidSubGrids(sudokuBoard: List<List<String>>): Boolean {
+    val steps = sqrt(sudokuBoard.size.toDouble()).toInt()
+    for (columnIndex in sudokuBoard[0].indices step steps) {
+        for (rowIndex in sudokuBoard.indices step steps) {
+            val seen = mutableListOf<String>()
+            val horizontalRange = columnIndex..columnIndex + (steps - 1)
+            val verticalRange = rowIndex..rowIndex + (steps - 1)
+            for (x in horizontalRange) {
+                for (y in verticalRange) {
+                    val cell = sudokuBoard[x][y]
+                    if (!isValidCell(cell, sudokuBoard.size, seen)) {
+                        return false
+                    } else {
+                        seen.add(cell)
+                    }
+                }
+            }
+        }
+    }
+    return true
+}
+
+fun isValidCell(cell: String, range: Int, seen: MutableList<String>): Boolean {
+    if (!isNumeric(cell) && cell != "-") return false
+    if (isNumeric(cell)) {
+        if (cell.toInt() !in 1..range) return false
+        if (seen.contains(cell)) return false
+    }
+    return true
+}
+
+fun isNumeric(input: String) = input.toIntOrNull() != null
+
